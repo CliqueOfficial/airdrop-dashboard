44a4ef02be3b2277e20b135dacf90ec925f976c7
Alan WANG <alan1995wang@outlook.com>
2025-10-16 01:27:58 +0800

    chore: use Show

---
+++ src/config.ts
@@ -0,0 +1,12 @@
+import { createConfig, http } from "wagmi";
+import { mainnet, sepolia, polygon, arbitrum } from "wagmi/chains";
+
+export const config = createConfig({
+    chains: [mainnet, sepolia, polygon, arbitrum],
+    transports: {
+        [mainnet.id]: http(),
+        [sepolia.id]: http(),
+        [polygon.id]: http(),
+        [arbitrum.id]: http(),
+    }
+});
