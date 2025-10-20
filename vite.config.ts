4977e145e9852b5c97a981b99f45aa0319d95570
Alan WANG <alan1995wang@outlook.com>
2025-10-20 23:01:54 +0800

    chore: add header

--- vite.config.ts
+++ vite.config.ts
@@ -7,6 +7,10 @@ export default defineConfig({
   server: {
     host: '0.0.0.0',
     port: 3000,
+    headers: {
+      'Cross-Origin-Opener-Policy': 'same-origin',
+      'Cross-Origin-Embedder-Policy': 'require-corp',
+    },
   },
   build: {
     target: 'esnext',
